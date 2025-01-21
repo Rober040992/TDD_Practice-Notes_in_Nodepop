741b9926108c3729982967fe4f46ff0dd700b0c7
RoberGF <rgfrasta@gmail.com>
2025-01-21 23:16:43 +0100

    testing using array

---
+++ examples/avg.js
@@ -0,0 +1,12 @@
+// Funcion para calcular la media de un array de valores
+function average(theArray) {
+    return theArray.average()
+}
+
+// 1. Para un array de valores 1, la media es 1. ([1, 1, 1]).
+// 2. Para un array vacio ([]) la media es NaN.
+// 3. Para un array de elementos positivos, la media debe ser positiva.
+// 4. Para un array de elementos negativos, la media debe ser negativa.
+
+
+module.exports = average
---
+++ examples/avg.test.js
@@ -0,0 +1,5 @@
+const average = require('./avg')
+
+describe('Para un array de valores 1, la media es 1. ([1, 1, 1])', () => {
+    
+})
