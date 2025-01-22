1dbc6e1002e42e1bccc825f0ac1f7f5aa5fed669
RoberGF <rgfrasta@gmail.com>
2025-01-22 11:58:57 +0100

    point 3 and 4 array elements test passed

--- examples/avg.js
+++ examples/avg.js
@@ -4,13 +4,13 @@ function calculateAverage(array) {
         return NaN
     }
     return array.reduce((acc, current) => acc + current) / array.length;
+    //return array.length === 0 ? NaN : array.reduce((acc, val) => acc + val) / array.length;
+
   }
   
-
 // 1. Para un array de valores 1, la media es 1. ([1, 1, 1]).
 // 2. Para un array vacio ([]) la media es NaN.
 // 3. Para un array de elementos positivos, la media debe ser positiva.
 // 4. Para un array de elementos negativos, la media debe ser negativa.
-
 
 module.exports = calculateAverage
--- examples/avg.test.js
+++ examples/avg.test.js
@@ -2,10 +2,18 @@ const calculateAverage = require('./avg')
 
 describe('Funcion para calcular la media de un array de valores', () => {
     test("1. Para un array de valores 1, la media es 1. ([1, 1, 1])", () => {
-        expect(calculateAverage([1, 1, 1])).toEqual(1);
+        expect(calculateAverage([1, 1, 1])).toBe(1);
     })
 
     test("2. Para un array vacio ([]) la media es NaN.", () => {
-        expect(calculateAverage([])).toEqual(NaN)
+        expect(calculateAverage([])).toBe(NaN)
+    })
+
+    test("3. Para un array de elementos positivos, la media debe ser positiva.", () => {
+        expect(calculateAverage([2, 2, 2, 2, 2, 2, 2])).toBe(2)
+    })
+
+    test("3. Para un array de elementos positivos, la media debe ser positiva.", () => {
+        expect(calculateAverage([-2, -20, -3,-5, -2, -2, -7, -2, -2])).toBe(-5)
     })
 })
